bd57cfbd0f8272f9c7a4ebadfb15811f444f7161
wahyuutomoputra <wahyuutomoputra@gmail.com>
2025-03-04 09:54:26 +0700

    pwa

---
+++ types/next-pwa.d.ts
@@ -0,0 +1,13 @@
+declare module 'next-pwa' {
+  import { NextConfig } from 'next'
+
+  type PWAConfig = {
+    dest: string
+    register: boolean
+    skipWaiting: boolean
+    disable?: boolean
+  }
+
+  function withPWA(config: PWAConfig): (nextConfig: NextConfig) => NextConfig
+  export = withPWA
+} 
